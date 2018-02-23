1b48275c142fcf3507f8c6e5c4ad1d337be713a1
Ahan Gupta <ahangupta.96@gmail.com>
2018-02-24 05:39:47 +0800

    Debugged routes

--- ecommerce/routes/payment.js
+++ ecommerce/routes/payment.js
@@ -1,8 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('payment' , { title: 'Payment' });
   }
 );
-
 
 module.exports = router;
