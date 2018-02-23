f0a9ff3742694cb63cf313141511a1db3ef06d3c
remo5000 <vigneshshankar@gmail.com>
2018-02-24 03:02:20 +0800

    clean working dir

--- ecommerce/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Connecting users through shipment' });
-});
-
-module.exports = router;
